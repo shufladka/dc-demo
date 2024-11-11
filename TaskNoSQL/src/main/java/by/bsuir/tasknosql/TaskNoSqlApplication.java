eb1aa8c3e638f5634a8a8f9a50d6979623841fd7
Klezovich <olegolegolegoleg88@gmail.com>
2024-11-11 20:55:07 +0300

    feat(task-330): initial commit

---
+++ TaskNoSQL/src/main/java/by/bsuir/tasknosql/TaskNoSqlApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.tasknosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNoSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskNoSqlApplication.class, args);
+    }
+
+}
