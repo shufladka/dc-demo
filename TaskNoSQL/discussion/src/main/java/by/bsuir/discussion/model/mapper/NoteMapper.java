b9605c4dedf2c860403e1b4c8cadaa7b3e63e414
Klezovich <olegolegolegoleg88@gmail.com>
2024-11-13 23:46:51 +0300

    feat(task-330): added discussion files

---
+++ TaskNoSQL/discussion/src/main/java/by/bsuir/discussion/model/mapper/NoteMapper.java
@@ -0,0 +1,22 @@
+package by.bsuir.discussion.model.mapper;
+
+import by.bsuir.discussion.model.dto.request.NoteRequestTo;
+import by.bsuir.discussion.model.dto.response.NoteResponseTo;
+import by.bsuir.discussion.model.entity.Note;
+import org.mapstruct.*;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface NoteMapper {
+
+    @Mapping(target = "key", expression = "java(new Note.Key(request.country(), request.id(), request.tweetId()))")
+    Note toEntity(NoteRequestTo request);
+
+    @Mapping(target = "key.country", source = "country")
+    @Mapping(target = "key.id", ignore = true)
+    @Mapping(target = "key.tweetId", source = "tweetId")
+    Note updateEntity(@MappingTarget Note entityToUpdate, NoteRequestTo updateRequest);
+
+    @Mapping(target = "id", source = "entity.key.id")
+    @Mapping(target = "tweetId", source = "entity.key.tweetId")
+    NoteResponseTo toResponseTo(Note entity);
+}
