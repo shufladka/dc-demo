2d8e76604c7a81dcd8d2f1836a240559337f60c8
Klezovich <olegolegolegoleg88@gmail.com>
2024-11-16 14:03:26 +0300

    feat(task-330): fixed services and yaml

--- TaskNoSQL/publisher/src/main/java/by/bsuir/publisher/client/discussion/config/DiscussionConfig.java
+++ TaskNoSQL/publisher/src/main/java/by/bsuir/publisher/client/discussion/config/DiscussionConfig.java
@@ -1,10 +1,10 @@
 package by.bsuir.publisher.client.discussion.config;
 
 import by.bsuir.publisher.client.discussion.DiscussionClient;
-import by.bsuir.publisher.util.config.ServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -13,14 +13,13 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class DiscussionConfig {
 
     @Autowired
-    private ServerConfig serverConfig;
+    private Environment env;
 
     @Bean
     RestClient restClient() {
-        String host = "http://127.0.0.1";
-        String url = host + ":" + serverConfig.getPort() + serverConfig.getServlet().getContextPath();
+        String defaultUrl = "http://localhost:24130/api/v1.0";
         return RestClient.builder()
-                .baseUrl("http://localhost:24130/api/v1.0")
+                .baseUrl(env.getProperty("rest-client.base-url", defaultUrl))
                 .build();
     }
 
