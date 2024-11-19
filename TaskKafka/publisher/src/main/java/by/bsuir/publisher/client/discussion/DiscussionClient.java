f94a659f5f6a70af069a8b6818261584e943d7b2
Klezovich <olegolegolegoleg88@gmail.com>
2024-11-19 23:47:42 +0300

    fix(task-340): solved issue with class naming

--- TaskKafka/publisher/src/main/java/by/bsuir/publisher/client/discussion/DiscussionClient.java
+++ TaskKafka/publisher/src/main/java/by/bsuir/publisher/client/discussion/DiscussionClient.java
@@ -1,6 +1,6 @@
 package by.bsuir.publisher.client.discussion;
 
-import by.bsuir.publisher.client.discussion.model.request.NoteRequestTo;
+import by.bsuir.publisher.client.discussion.model.request.DiscussionRequestTo;
 import by.bsuir.publisher.model.dto.response.NoteResponseTo;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 public interface DiscussionClient {
-    NoteResponseTo save(@RequestBody @Valid NoteRequestTo entity);
+    NoteResponseTo save(@RequestBody @Valid DiscussionRequestTo entity);
     List<NoteResponseTo> findAll(@RequestParam(defaultValue = "0") Integer pageNumber,
                                  @RequestParam(defaultValue = "5") Integer pageSize);
     NoteResponseTo findById(@PathVariable Long id);
-    NoteResponseTo update(@RequestBody @Valid NoteRequestTo entity);
+    NoteResponseTo update(@RequestBody @Valid DiscussionRequestTo entity);
     void delete(@PathVariable @Valid Long id);
 }
