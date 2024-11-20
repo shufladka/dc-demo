4d86b7ac7407c0300ba34c5b2d603e6a588f4c5a
Klezovich <olegolegolegoleg88@gmail.com>
2024-11-20 17:47:53 +0300

    feat(task-350): initial commit

---
+++ TaskRedis/src/main/java/by/bsuir/taskredis/TaskRedisApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.taskredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRedisApplication.class, args);
+    }
+
+}
